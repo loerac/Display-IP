901af69f926345085299dfd8c1641479b5c0223f
Christian Loera <chatoloera18@gmail.com>
2018-02-14 05:19:02 -0600

    Created installation.sh

---
+++ installation.sh
@@ -0,0 +1,6 @@
+# Running this command will setup and install the requirements to use PaPiRus
+# While it is running, you will be asked for the python version you will be using, 2 or 3
+# You'll also be ask to enter the size of the screen you're using: 1.44" - 2.7" display
+# When the script is complete, you'll be asked to reboot the Raspberry Pi
+
+curl -sSL https://pisupp.ly/papiruscode | sudo bash
